e5a9a2e7a712865ab918ed561c7baf1507cfa532
Max Bergman <max.bergman@hotmail.com>
2023-02-20 22:37:00 +0100

    Guess states of the US

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ while len(guessed_states) < 50:
     answer = answer.title()
 
     if answer == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("Missing_states.csv")
         break
